47dbc1dd024fe376503b1acea3cbc41b83e9df8e
Jeroen Seghers <jeroen@seghers.com>
2022-09-06 14:23:00 +0100

    fix: getParameterByName now returns undefined if param not found

--- js/site-utils.js
+++ js/site-utils.js
@@ -23,8 +23,8 @@ function getParameterByName(name, url = window.location.href) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
+  if (!results) return undefined;
+  if (!results[2]) return undefined;
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
