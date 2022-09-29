e23516734a110eacaa6d7eb8afad22487ba10e00
Jeroen Seghers <jeroen@seghers.com>
2022-09-29 16:22:24 +0100

    Update site-support.js

--- js/site-support.js
+++ js/site-support.js
@@ -3,6 +3,6 @@ $(() => {
 
   var p1 = "bWFpbHRvOmZlZWRiYWNrQA==";
   var p2 = atob(p1) + "getupnext.com";
-  $(".getintouch").attr('href','mailto:'+p2);
-  $("#supportEmail").text(p2);
+  $(".getintouch").attr('href',p2);
+  $("#supportEmail").text(p2.replace('mai'+'lto:','');
 });
