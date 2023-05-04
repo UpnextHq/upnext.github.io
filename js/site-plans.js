caf8813f3333873b6abbd85f2502e26a81452159
Jeroen Seghers <jeroen@seghers.com>
2023-05-04 12:44:34 +0100

    feat: no-cc-trial support

--- js/site-plans.js
+++ js/site-plans.js
@@ -4,13 +4,26 @@ var coupon = getParameterByName("coupon");
 var trial = getParameterByName("trial");
 var host = getParameterByName("host") ?? "app.getupnext.com";
 var source = getParameterByName("source");
+var nocctrial = getParameterByName("nocctrial");
+var trialexpired = getParameterByName("trialexpired");
+
 
 trackEvent("page.selectPlan", { plan, coupon });
 
 var trialEnabled = trial || localStorage.getItem("trial") || true;
 
+var noccEnabled = nocctrial || localStorage.getItem("nocctrial") || false;
+
 if (trialEnabled) {
   $("#plans-title").text("Start your 14-day free trial!");
+}
+
+if (noccEnabled) {
+  if (trialexpired) {
+    $("#plans-title").text("Susbcribe to continue using Upnext!");
+  } else{
+    $("#plans-title").text("Subscribe to Upnext today!");
+  }
 }
 
 const updateSelectedPlan = (newPlan) => {
@@ -38,6 +51,8 @@ const handleContinue = () => {
       (coupon || localStorage.getItem("coupon")) +
       "&trial=" +
       trialEnabled +
+      "&nocctrial=" +
+      noccEnabled +
       (source ? "&source=" + source : "") +
       "&utm_campaign=" +
       localStorage.getItem("utm_campaign") +
